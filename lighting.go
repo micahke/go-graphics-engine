cd3751e0b95eeb439afa6aa7f499500cbed1ea99
yumyumcurryman <micahkelias@gmail.com>
2023-05-14 20:48:37 -0400

    test

--- lighting.go
+++ lighting.go
@@ -107,6 +107,7 @@ func RunLighting() {
 		objectShader.SetUniform3f("light.diffuse", 0.5, 0.5, 0.5)
 		objectShader.SetUniform3f("light.specular", 1.0, 1.0, 1.0)
 
+<<<<<<< HEAD
 		// Light colors
 		// l_LightColor[0] = float32(math.Sin(glfw.GetTime() * 2.0))
 		// l_LightColor[1] = float32(math.Sin(glfw.GetTime() * 0.7))
@@ -119,6 +120,17 @@ func RunLighting() {
 		// Set light values in the component
 		objectShader.SetUniform3f("light.ambient", ambientColor[0], ambientColor[1], ambientColor[2])
 		objectShader.SetUniform3f("light.diffuse", diffuseColor[0], diffuseColor[1], diffuseColor[2])
+=======
+		lightColor[0] = float32(math.Sin(glfw.GetTime() * 2.0))
+		lightColor[1] = float32(math.Sin(glfw.GetTime() * 0.7))
+		lightColor[2] = float32(math.Sin(glfw.GetTime() * 1.3))
+
+    diffuseColor := lightColor.Mul(0.5)
+    ambientColor := diffuseColor.Mul(0.2)
+
+    objectShader.SetUniform3f("light.ambient", ambientColor[0], ambientColor[1], ambientColor[2])
+    objectShader.SetUniform3f("light.diffuse", diffuseColor[0], diffuseColor[1], diffuseColor[2])
+>>>>>>> parent of 8d9b60b (added light color to light cube)
 
 		l_Camera.Update(&objectShader)
 
@@ -139,7 +151,10 @@ func RunLighting() {
 		lightScale := glm.Scale3D(0.2, 0.2, 0.2)
 		lightCube = lightCube.Mul4(lightTranslation).Mul4(lightScale)
 		lightShader.SetUniformMat4f("model", lightCube)
+<<<<<<< HEAD
 		lightShader.SetUniform3f("cubeColor", l_LightColor[0], l_LightColor[1], l_LightColor[2])
+=======
+>>>>>>> parent of 8d9b60b (added light color to light cube)
 		renderer.Draw(*lightCubeVAO, lightShader)
 
 		window.SwapBuffers()
